634f96fe66ec7209faaa8c00d78a4fe21eeaf270
takahashi14305 <117267359+takahashi14305@users.noreply.github.com>
2023-03-26 03:06:21 +0000

    user controller g

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
   devise_for :users
   root to: 'homes#top'
   resources :post_images, only:[:new, :create, :index, :show, :destroy]
