2f30ef1cbdeaebb6675f362867ba62fd362520ee
takahashi14305 <117267359+takahashi14305@users.noreply.github.com>
2023-03-28 08:50:46 +0000

    comment ok

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -10,6 +10,6 @@ class PostCommentsController < ApplicationController
   private
 
   def post_comment_params
-    params.require(:post_comments).permit(:comment)
+    params.require(:post_comment).permit(:comment)
   end
 end
