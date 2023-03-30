adc86f64b835dded437285c8711b16a5650f2079
takahashi14305 <117267359+takahashi14305@users.noreply.github.com>
2023-03-30 09:30:49 +0000

    favorited_by? method

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,12 +3,16 @@ class PostImage < ApplicationRecord
   belongs_to :user
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+
   def get_image(width, height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     image.variant(resize_to_limit: [width, height]).processed
+  end
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
   end
 end
