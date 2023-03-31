f4abdeab9b7781a8159492e4966ba99cfb445898
takahashi14305 <117267359+takahashi14305@users.noreply.github.com>
2023-03-31 11:16:59 +0000

    favorite d controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -8,5 +8,9 @@ class FavoritesController < ApplicationController
   end
 
   def destroy
+    post_image = PostImage.find(params[:post_image_id])
+    favorite = current_user.favorites.find_by(post_image_id: post_image.id)
+    favorite.destroy
+    redirect_to post_image_path(post_image)
   end
 end
