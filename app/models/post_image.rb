795c49e344b9de9d9db5cda905622f1032cd62ab
takahashi14305 <117267359+takahashi14305@users.noreply.github.com>
2023-04-03 10:24:04 +0000

    validates update

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,9 +4,9 @@ class PostImage < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
-  varidates :shop_name, presence: true
-  varidates :caption, presence: true
-  varidates :image, presence: true
+  validates :shop_name, presence: true
+  validates :caption, presence: true
+  validates :image, presence: true
 
   def get_image(width, height)
     unless image.attached?
