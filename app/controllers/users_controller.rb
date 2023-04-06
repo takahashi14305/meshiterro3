3e3d1cbeb548cc65a9dc5244a3f6e42149fafbac
takahashi14305 <117267359+takahashi14305@users.noreply.github.com>
2023-04-06 09:51:13 +0000

    is_matching_login_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,12 @@ class UsersController < ApplicationController
   end
 
   def edit
+    is_matching_login_user
     @user = User.find(params[:id])
   end
 
   def update
+    is_matching_login_user
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(current_user)
@@ -18,6 +20,13 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :profile_image)
+  end
+
+  def is_matching_login_user
+    user_id = params[:id].to_i
+    unless user_id == current_user.id
+      redirect_to post_images_path
+    end
   end
 
 end
