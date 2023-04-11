9deacfcb4e122f56982f1d7fd55a63374e696d0a
takahashi14305 <117267359+takahashi14305@users.noreply.github.com>
2023-04-11 01:14:57 +0000

    redirect d

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -4,14 +4,14 @@ class PostCommentsController < ApplicationController
     comment = current_user.post_comments.new(post_comment_params)
     comment.post_image_id = post_image.id
     comment.save
-    redirect_to post_image_path(post_image)
+    #redirect_to post_image_path(post_image)
   end
 
   def destroy
     PostComment.find(params[:id]).destroy
-    redirect_to post_image_path(params[:post_image_id])
+    #redirect_to post_image_path(params[:post_image_id])
   end
-  
+
   private
 
   def post_comment_params
