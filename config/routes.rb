8d82a018fbfea63a83117b506754af318ae40e7f
takahashi14305 <117267359+takahashi14305@users.noreply.github.com>
2023-04-12 08:02:26 +0000

    routes update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  resources :post_images, only:[:new, :create, :index, :show, :destroy] do
+  resources :post_images, only:[:new, :create, :index, :show, :destroy, :edit, :update] do
     resources :post_comments, only:[:create, :destroy]
     resource :favorites, only:[:create, :destroy]
   end
