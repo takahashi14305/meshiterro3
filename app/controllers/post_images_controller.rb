a6a539bdfefc0de57d2930c73e1ca2af7c29a9d8
takahashi14305 <117267359+takahashi14305@users.noreply.github.com>
2023-04-13 04:48:01 +0000

    controller

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -22,7 +22,14 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.find(params[:id])
     @post_comment = PostComment.new
   end
-
+  
+  def edit
+    
+  end
+  
+  def update
+  end
+  
   def destroy
     @post_image = PostImage.find(params[:id])
     @post_image.destroy
