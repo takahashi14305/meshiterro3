53ae0547330a7ec0d1de15c40974c087b71ea50f
takahashi14305 <117267359+takahashi14305@users.noreply.github.com>
2023-04-27 08:56:50 +0000

    user model relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
 
   def get_profile_image(width, height)
     unless profile_image.attached?
