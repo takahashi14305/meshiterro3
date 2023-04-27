56be963bc236f21f2379dec5a9526fc2321261f2
takahashi14305 <117267359+takahashi14305@users.noreply.github.com>
2023-04-27 08:58:07 +0000

    user model relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
 
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
 
   def get_profile_image(width, height)
     unless profile_image.attached?
