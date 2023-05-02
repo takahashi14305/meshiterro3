0c6b514a2ef0486e0e0081f040e999dd9409ef36
takahashi14305 <117267359+takahashi14305@users.noreply.github.com>
2023-05-02 13:09:36 +0000

    followings routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   end
   resources :users, only:[:show, :edit, :update] do
     resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
   end
   get  "about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
