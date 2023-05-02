32d090f9303325e3582b6661ad0ae1053b033419
takahashi14305 <117267359+takahashi14305@users.noreply.github.com>
2023-05-02 13:08:56 +0000

    relationship cont

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
     resources :post_comments, only:[:create, :destroy]
     resource :favorites, only:[:create, :destroy]
   end
-  resources :users, only:[:show, :edit, :update]
+  resources :users, only:[:show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+  end
   get  "about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
